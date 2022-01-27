8c8d69c998c236b69b00418ddd34277186a1ac59
Gustavo Bomfim <gustavobdev@gmail.com>
2022-01-27 12:33:43 -0300

    Criando o prodConfig

---
+++ src/main/java/com/gbs/easyfreight/config/ProdConfig.java
@@ -0,0 +1,35 @@
+package com.gbs.easyfreight.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gbs.easyfreight.services.DBService;
+import com.gbs.easyfreight.services.EmailService;
+import com.gbs.easyfreight.services.SmptEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+
+	@Bean
+	public EmailService emailService() {
+		return new SmptEmailService();
+	}
+	
+}
